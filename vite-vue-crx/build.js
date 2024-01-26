c0418b833aefec772c1950054ad7951f1943f295
Qian Zhiqiang <qianzq@touzhiwang.com>
2024-01-26 17:08:24 +0800

    fix: 文件内容复制粘贴错误

--- vite-vue-crx/build.js
+++ vite-vue-crx/build.js
@@ -1,28 +1,59 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import fs from "fs";
 import path from "path";
-import { CRX_BACKGROUND_OUTDIR } from "./globalConfig";
+import {
+  CRX_OUTDIR,
+  CRX_CONTENT_OUTDIR,
+  CRX_BACKGROUND_OUTDIR,
+} from "./globalConfig.js";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    // 输出目录
-    outDir: CRX_BACKGROUND_OUTDIR,
-    lib: {
-      entry: [path.resolve(__dirname, "src/background/index.js")],
-      // background script不支持ES6，因此不用使用es模式，需要改为cjs模式
-      formats: ["cjs"],
-      // 设置生成文件的文件名
-      fileName: () => {
-        // 将文件后缀名强制定为js，否则会生成cjs的后缀名
-        return "background.js";
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  plugins: [vue()],
-});
+// 拷贝目录文件
+const copyDirectory = (srcDir, destDir) => {
+  // 判断目标目录是否存在，不存在则创建
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir);
+  }
+
+  fs.readdirSync(srcDir).forEach((file) => {
+    const srcPath = path.join(srcDir, file);
+    const destPath = path.join(destDir, file);
+
+    if (fs.lstatSync(srcPath).isDirectory()) {
+      // 递归复制子目录
+      copyDirectory(srcPath, destPath);
+    } else {
+      // 复制文件
+      fs.copyFileSync(srcPath, destPath);
+    }
+  });
+};
+
+// 删除目录及文件
+const deleteDirectory = (dir) => {
+  if (fs.existsSync(dir)) {
+    fs.readdirSync(dir).forEach((file) => {
+      const curPath = path.join(dir, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // 递归删除子目录
+        deleteDirectory(curPath);
+      } else {
+        // 删除文件
+        fs.unlinkSync(curPath);
+      }
+    });
+    // 删除空目录
+    fs.rmdirSync(dir);
+  }
+};
+
+// 源目录：content script临时生成目录
+const contentOutDir = path.resolve(process.cwd(), CRX_CONTENT_OUTDIR);
+// 源目录：background script临时生成目录
+const backgroundOutDir = path.resolve(process.cwd(), CRX_BACKGROUND_OUTDIR);
+// 目标目录：Chrome Extension 最终build目录
+const outDir = path.resolve(process.cwd(), CRX_OUTDIR);
+// 将复制源目录内的文件和目录全部复制到目标目录中
+copyDirectory(contentOutDir, outDir);
+copyDirectory(backgroundOutDir, outDir);
+// 删除源目录
+deleteDirectory(contentOutDir);
+deleteDirectory(backgroundOutDir);
